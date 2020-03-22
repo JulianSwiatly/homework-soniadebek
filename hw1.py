94006308a6f560854c70dd21598ab3ee29d983f9
soniadebek <62251464+soniadebek@users.noreply.github.com>
2020-03-22 20:36:17 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -8,25 +8,20 @@ CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-1
 confirmed_cases = pd.read_csv(CONFIRMED_CASES_URL, error_bad_lines=False)
 
 
-def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
-  data = datetime.date(year,month,day)
-  data1 = data.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
-  polska = confirmed_cases.loc[confirmed_cases["Country/Region"]=="Poland"]
-  result = polska[data1].values[0]
-  return result
 
-
+def poland_cases_by_date(day, month, year: int = 2020) -> int:
+  y = year % 100
+  return confirmed_cases.loc[confirmed_cases["Country/Region"]=="Poland"][f'{month}/{day}/{y}'].values[0]
 
 def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
-  data = datetime.date(year,month,day)
-  data1 = data.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
-  kraje = confirmed_cases[["Country/Region", data1]].groupby(["Country/Region"]).sum().sort_values(by=data1, ascending=False).head(5)
-  return list(kraje.index)
+  d = datetime.date(year, month, day)
+  d1 = d.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0","/")
+  countries = confirmed_cases[["Country/Region", d1]].groupby(["Country/Region"]).sum().sort_values(by=d1, ascending=False).head(5)
+  return list(countries.index)
 
-# Function name is wrong, read the pydoc
 def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
-  data = datetime.date(year,month,day)
-  data1 = data.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
-  wczoraj = data - datetime.timedelta(days=1)
-  wczorajstr = wczoraj.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0", "/")
-  return len(confirmed_cases.loc[confirmed_cases[data1]-confirmed_cases[wczorajstr]!=0].index)
+  d = datetime.date(year, month, day)
+  d2 = d.strftime('%m/%d/%y').lstrip("0").replace(" 0", " ").replace("/0","/")
+  wczoraj = d - datetime.timedelta(days=1)
+  wczoraj_str = wczoraj.strftime('%m/%d/%y').lstrip("0").replace(" 0"," ").replace("/0","/")
+  return len(confirmed_cases.loc[confirmed_cases[d2] - confirmed_cases[wczoraj_str]!=0].index
